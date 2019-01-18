b47cda31ea03959d91db1ce07c836445bdce8ac3
szlangini <vincent.szlang@googlemail.com>
2019-01-18 15:52:29 +0100

    added listAdapter in custom

--- app/src/main/java/com/szlangini/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/szlangini/example/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.szlangini.example.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.szlangini.example.coderswag.Adapters.CategoryAdapter
 import com.szlangini.example.coderswag.Model.Category
 import com.szlangini.example.coderswag.R
 import com.szlangini.example.coderswag.Services.DataService
@@ -10,16 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter<Category>(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories
-            )
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
 
